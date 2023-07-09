3269f4cd96afe63df3f2c365b5bed505bcb77648
Bhuwan <bhuwanjsh02@gmail.com>
2023-07-09 08:00:41 +0530

    deleted

--- .history/Server/routes/cloudinary.routes_20230708193450.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const cloudinary = require("cloudinary").v2;
-
-router.route("/uploadImages").post(async (req, res) => {});
-export default router;
--- .history/Server/routes/course.routes_20230708194049.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-router.route("/upload").post(async (req, res) => {});
-
-module.exports = router;
